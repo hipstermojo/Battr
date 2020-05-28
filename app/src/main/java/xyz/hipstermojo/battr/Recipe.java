94f3ac200a7ec957333db18fff4b580c97915fbc
Kenneth Gitere <gitere81@gmail.com>
2020-05-28 11:36:08 +0300

    Extract additional information from API for recipe information and populate the recipe detail view with more of that data using minimal styling

--- app/src/main/java/xyz/hipstermojo/battr/Recipe.java
+++ app/src/main/java/xyz/hipstermojo/battr/Recipe.java
@@ -5,15 +5,31 @@ import android.os.Parcelable;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class Recipe implements Parcelable {
     private static final String baseImageUrl = "https://spoonacular.com/recipeImages/";
-    private String id;
+    private int id;
+    @SerializedName("readyInMinutes")
+    private int duration;
     private String image;
+    @SerializedName("extendedIngredients")
+    private List<Ingredient> ingredients;
+    @SerializedName("analyzedInstructions")
+    private List<Instruction> instructions;
+    private int servings;
+    @SerializedName("sourceName")
+    private String source;
+    private String sourceUrl;
     private String title;
 
+
     protected Recipe(Parcel in) {
-        id = in.readString();
+        id = in.readInt();
+        duration = in.readInt();
         image = in.readString();
+        servings = in.readInt();
+        sourceUrl = in.readString();
         title = in.readString();
     }
 
@@ -29,6 +45,10 @@ public class Recipe implements Parcelable {
         }
     };
 
+    public int getId() {
+        return id;
+    }
+
     public String getImage() {
         if (!this.image.startsWith("http")){
             this.image =  String.format("%s%s-480x360.%s", baseImageUrl, id, this.image.split("\\.")[1]);
@@ -40,6 +60,30 @@ public class Recipe implements Parcelable {
         return title;
     }
 
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getSourceUrl() {
+        return sourceUrl;
+    }
+
+    public List<Instruction> getInstructions() {
+        return instructions;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -47,8 +91,25 @@ public class Recipe implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(this.id);
+        dest.writeInt(this.id);
+        dest.writeInt(this.duration);
         dest.writeString(this.image);
+        dest.writeInt(this.servings);
+        dest.writeString(this.sourceUrl);
         dest.writeString(this.title);
+    }
+
+    class Instruction {
+        public List<Step> steps;
+    }
+
+    class Step {
+        String step;
+        int number;
+    }
+
+    class Ingredient {
+        String name;
+        double amount;
     }
 }
--- app/src/main/java/xyz/hipstermojo/battr/RecipeDetailActivity.java
+++ app/src/main/java/xyz/hipstermojo/battr/RecipeDetailActivity.java
@@ -1,30 +1,89 @@
 package xyz.hipstermojo.battr;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.picasso.Picasso;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RecipeDetailActivity extends AppCompatActivity {
-    private ImageView recipeImage;
-    private TextView recipeTitle;
+
+    private Recipe recipe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+        recipe = getIntent().getParcelableExtra(MainActivity.RECIPE);
+        insertRecipeInfo();
 
-        recipeImage = findViewById(R.id.recipe_view_image);
-        recipeTitle = findViewById(R.id.recipe_view_name);
+        ImageView recipeImage = findViewById(R.id.recipe_view_image);
+        TextView recipeTitle = findViewById(R.id.recipe_view_name);
+        TextView recipeUrl = findViewById(R.id.recipe_view_url);
 
-        Recipe recipe = getIntent().getParcelableExtra(MainActivity.RECIPE);
         Picasso.get()
                 .load(recipe.getImage()).fit()
                 .centerCrop()
                 .into(recipeImage);
 
         recipeTitle.setText(recipe.getTitle());
+        recipeUrl.setText(recipe.getSourceUrl());
+    }
+
+
+    private void insertRecipeInfo() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.spoonacular.com")
+                .addConverterFactory(GsonConverterFactory.create()).build();
+        RecipeService recipeService = retrofit.create(RecipeService.class);
+        recipeService.fetchRecipeInfo(recipe.getId(), BuildConfig.SpoonacularAPIKey).enqueue(new Callback<Recipe>() {
+            @Override
+            public void onResponse(Call<Recipe> call, Response<Recipe> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("RETROFIT", String.format("Call to get recipe information was unsuccessful. Status: %d", response.code()));
+                } else {
+                    if (response.body() != null) {
+                        recipe = response.body();
+                        TextView recipeSource = findViewById(R.id.recipe_view_source);
+                        TextView recipeServings = findViewById(R.id.recipe_view_servings);
+                        TextView recipeDuration = findViewById(R.id.recipe_view_duration);
+
+                        LinearLayout recipesIngredientsView = findViewById(R.id.recipe_view_ingredients_view);
+                        LinearLayout recipesInstructionsView = findViewById(R.id.recipe_view_instructions_view);
+
+                        recipeSource.setText(String.format("by %s", recipe.getSource()));
+                        recipeServings.setText(String.format("Serves %d", recipe.getServings()));
+                        recipeDuration.setText(String.format("%d min", recipe.getDuration()));
+
+                        for (Recipe.Ingredient ingredient : recipe.getIngredients()){
+                            TextView ingredientTextView = new TextView(getApplicationContext());
+                            ingredientTextView.setText(ingredient.name);
+                            recipesIngredientsView.addView(ingredientTextView);
+                        }
+
+                        for (Recipe.Step step : recipe.getInstructions().get(0).steps){
+                            TextView stepTextView = new TextView(getApplicationContext());
+                            stepTextView.setText(step.step);
+                            recipesInstructionsView.addView(stepTextView);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Recipe> call, Throwable t) {
+                Log.e("RETROFIT", String.format("Call to get recipe information failed \n%s", t.getMessage()));
+            }
+        });
     }
 }
